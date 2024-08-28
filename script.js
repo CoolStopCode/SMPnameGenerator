1590b914b6b15431dc9df6278af8652803c6c9ac
CoolStopCode <coolstopmail@gmail.com>
2024-08-28 16:32:15 +0000

    Your commit message

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+alert("Script working!")
 
+const genbutton = document.getElementById("generate");
+const outputdiv = document.getElementById("output");
+
+genbutton.addEventListener("click", generate);
+
+function generate() {
+    const randomPrefix = prefix[Math.floor(Math.random() * prefix.length)];
+    const randomSuffix = suffix[Math.floor(Math.random() * suffix.length)];
+
+    const output = randomPrefix + " " + randomSuffix;
+
+    outputdiv.innerText = output;
+}
